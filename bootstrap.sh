f2eefbec738c605c1a2e2b893738e7275b210edb
Azat Khuzhin <a3at.mail@gmail.com>
2018-09-16 20:25:26 +0300

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+function path() { echo "$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")/$*"; }
+
+function make_rootfs()
+{
+    local img="$1"
+    shift
+
+    fallocate -l5G "$img"
+    mke2fs -F -t ext4 "$img" "$@"
+}
+function configure_locales()
+{
+    local mnt="$1"
+    shift
+
+    echo "en_US.UTF-8 UTF-8" | sudo tee -a "$mnt"/etc/locale.gen
+    sudo chroot mnt locale-gen
+}
+function configure_rootfs()
+{
+    local mnt="$1"
+    shift
+
+    sudo mkdir -p "$mnt"/var/lib/pacman
+
+    local pkgs=(
+        # base
+        base bash glibc coreutils e2fsprogs which procps-ng util-linux
+        findutils grep gawk vi less sed
+        bzip2 gzip tar
+        pciutils
+        # net
+        bind-tools dhcp dhcpcd inetutils iproute2 iputils
+        # debug
+        strace tcpdump perf
+        # stuff
+        tmux
+    )
+    local opts=(
+        # we have our own kernel
+        --ignore linux*
+        # but some packages requires api headers and without this there will
+        # endless loop for resolving dependency
+        --assume-installed linux-api-headers=$(uname -r)
+
+        # ignore gnupg, since this interacts with main dirmngr and send a lot
+        # of invalid commands to it:
+        #     read(3, "ERR 167772435 Unknown IPC command <Dirmngr>\nERR 167772435 Unknown IPC command <Dirmngr>\nERR 167772435 Unknown IPC command <Dirmngr>\nERR 167772435 Unknown IPC command <Dirmngr>\nERR 167772435 Unknown IPC command <Dirmngr>\nERR 167772435 Unknown IPC command <Dirmngr>\nERR 167772435 Unknown IPC command <Dirmngr>\nERR 167772435 Unknown IPC command <Dirmngr>\nERR 167772435 Unknown IPC command <Dirmngr>\n", 1002) = 396
+        #     write(4, "ERR 167772435 Unknown IPC command <Dirmngr>", 43) = 43
+        #     write(4, "\n", 1)                       = 1
+        #     write(4, "ERR 167772435 Unknown IPC command <Dirmngr>", 43) = 43
+        #     write(4, "\n", 1)                       = 1
+        --ignore gnupg
+        --ignore gpgme
+        --assume-installed gpgme=1.11.1-2
+
+        -r "$mnt"
+    )
+    sudo pacman -Sy "${opts[@]}" "${pkgs[@]}"
+
+    sudo cp $(path init) mnt/sbin/
+}
+function main()
+{
+    make_rootfs rootfs.img || return 1
+
+    mkdir -p mnt
+    sudo mount -o loop rootfs.img mnt
+    configure_rootfs mnt
+    configure_locales mnt
+    sudo umount mnt
+}
+main "$@"
---
+++ qemu.sh
@@ -0,0 +1,183 @@
+#!/usr/bin/env bash
+
+function abspath() { echo "$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")/$*"; }
+
+function find_kernel()
+{
+    local paths=(
+        /src/linux/arch/x86/boot/bzImage
+    )
+
+    for path in "${paths[@]}"; do
+        [ -f $path ] && {
+            echo $path
+            return
+        }
+    done
+}
+
+function qemu()
+{
+    local kernel_opts=(
+        console=ttyS0
+        root=/dev/sda
+        raid=noautodetect
+        panic_on_warn=1
+        panic=-1
+        intel_iommu=on
+        nokaslr
+
+        "${kernel_args[@]}"
+    )
+
+    local cpus=$(getconf _NPROCESSORS_ONLN)
+    local qemu_opts=(
+        -enable-kvm
+        -machine q35,accel=kvm
+        -device intel-iommu
+        -name linux-dev
+
+        -no-reboot
+        -s
+
+        -nographic
+        -serial mon:stdio
+
+        # snapshot by default, <Ctrl-T s> to manually flush
+        -drive if=ide,file="$(abspath rootfs.img)",snapshot=on
+
+        -kernel "$(find_kernel)"
+        -append "${kernel_opts[*]}"
+
+        # <Ctrl-T> as an escape character
+        # (since default <Ctrl-A> is tmux control-key)
+        -echr 0x14
+
+        -smp $(( cpus/2 ))
+        -m 1G
+    )
+    qemu-system-x86_64 "${qemu_opts[@]}" "$@"
+}
+
+function make_spare_drive()
+{
+    local i="$1" size="$2"
+    shift 2
+
+    local path="$(abspath "$i.img")"
+    fallocate -l "$size" "$path"
+    echo "$path"
+}
+function make_spare_drive_in_mem()
+{
+    local i="$1" size="$2"
+    shift 2
+
+    local path="/dev/shm/$i.img"
+    fallocate -l "$size" "$path"
+    echo "$path"
+}
+function make_drive()
+{
+    local path="$1"
+    shift
+
+    local base="$(basname "$path")"
+    fallocate -l1G "$base.img"
+    mke2fs -F -t ext4 "$base.img"
+    mkdir -p "$mnt.mnt"
+    sudo mount -o loop "$base.img" "$mnt.mnt"
+    rsync -a "$path" "$mnt.mnt"
+    sudo umount "$mnt.mnt"
+
+    echo "$base.img"
+}
+function parse_drives()
+{
+    local i
+    for i in "$@"; do
+        # it is likely some qemu option
+        if [ "${i:0:1}" == "-" ]; then
+            qemu_args+=( "$i" )
+            continue
+        fi
+
+        if [ -f "$i" ]; then
+            qemu_args+=( "-drive" "if=ide,file=$i" )
+            continue
+        fi
+
+        if [ -d "$i" ]; then
+            local drive="$(make_drive "$i")"
+            qemu_args+=( "-drive" "if=ide,file=$drive" )
+            continue
+        fi
+
+        echo "Unknown option: $i" >&2
+        return 2
+    done
+}
+
+declare -A opts
+function usage()
+{
+    cat <<EOL
+  $0 [ OPTS ] -- [ DRIVE/PATH/... ] [ QEMU OPTS ]
+
+Options:
+  -S <size of spare drive>
+  -M <size of spare drive in memory>
+  -K <kernel arg, cmdline>
+EOL
+}
+function parse_opts()
+{
+    local OPTARG OPTIND c
+
+    qemu_args=()
+    kernel_args=()
+    spare_drives=()
+    spare_drives_in_mem=()
+
+    cleanup_images=()
+
+    while getopts "hS:M:K:" c; do
+        case "$c" in
+            S) spare_drives+=($OPTARG);;
+            M) spare_drives_in_mem+=($OPTARG);;
+            K) kernel_args+=($OPTARG);;
+            h) usage && exit 0;;
+            *) usage >&2 && exit 1;;
+        esac
+    done
+
+    for ((i = 0; i < ${#spare_drives[@]}; ++i)); do
+        local size="${spare_drives[$i]}"
+        local img="$(make_spare_drive $i $size)"
+        cleanup_images+=( "$img" )
+        qemu_args+=(
+            "-drive"
+            "if=ide,file=$img,snapshot=on"
+        )
+    done
+
+    for ((i = 0; i < ${#spare_drives_in_mem[@]}; ++i)); do
+        local size="${spare_drives_in_mem[$i]}"
+        local img="$(make_spare_drive_in_mem $i $size)"
+        cleanup_images+=( "$img" )
+        qemu_args+=(
+            "-drive"
+            "if=ide,file=$img,snapshot=on"
+        )
+    done
+
+    shift $(( OPTIND-1 ))
+    parse_drives "$@" || return
+}
+function main()
+{
+    parse_opts "$@" || return
+    qemu "${qemu_args[@]}"
+    rm -fr "${cleanup_images[@]?}"
+}
+main "$@"
